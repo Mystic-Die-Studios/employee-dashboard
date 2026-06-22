3cb7fa098465b3974d6a201d586fedc2a0a8c11f
kamivision <kamivision@gmail.com>
2026-06-22 18:25:54 -0500

    feat: initial feature/auth branch commit

--- server/user_app/models.py
+++ server/user_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+class Employee(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    github_username = models.CharField(max_length=100)
