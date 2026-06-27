c302943ddfce8b1d1bf5cefefbf5dd1f0e3801a8
kamivision <kamivision@gmail.com>
2026-06-26 19:01:37 -0500

    fix: create/admin name mismatch

--- server/user_app/urls.py
+++ server/user_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create/', CreateUserView.as_view(), name='create_user'),
+    path('create/admin/', CreateAdminUserView.as_view(), name='create_admin_user'),
     path('github/callback/', GitHubCallBackView.as_view(), name='github_callback'),
 ]
